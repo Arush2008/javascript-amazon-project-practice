08a3fc9fb99c650d27ee5721dcb81854fc96ee68
Arush2008 <23779@burnside.school.nz>
2024-12-21 01:00:02 +1300

    fixed Checkout page

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
-export function formateCurrency(priceCents) {
-    return (priceCents/100).toFixed(2);
-}
+export function formatCurrency(priceCents) {
+    return (priceCents / 100).toFixed(2);
+  }
